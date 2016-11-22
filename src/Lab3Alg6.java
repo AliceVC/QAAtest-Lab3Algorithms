b0f2f7966449c2fc9d49a8249271a726a76429f5
flo <flo.macicasan@gmail.com>
2016-11-22 23:48:56 +0200

    Adds iterative conversion from F to C

--- src/Lab3Alg6.java
+++ src/Lab3Alg6.java
@@ -6,9 +6,17 @@ public class Lab3Alg6 {
     public static void main(String[] args) {
         int f = Integer.valueOf(ReadUtils.readValueFromConsole());
 
-        double c = 5.0/9 * (f - 32);
 
+        double c = convertFToC(f);
+
+        for(int i = 0; i < f; i += 10 ) {
+            System.out.println("F" + i + " in C " + convertFToC(i));
+        }
         System.out.println(f + " in celsius " + c);
 
+    }
+
+    public static double convertFToC(int f) {
+        return 5.0/9 * (f - 32);
     }
 }
