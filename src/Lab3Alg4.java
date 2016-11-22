e75dc9a96df7f843c88f513f44eefb6e499102bd
flo <flo.macicasan@gmail.com>
2016-11-22 21:42:35 +0200

    multiply 2 numbers

---
+++ src/Lab3Alg4.java
@@ -0,0 +1,26 @@
+/**
+ * @author flo
+ * @since 22/11/2016.
+ */
+public class Lab3Alg4 {
+    public static void main(String[] args) {
+        int a = Integer.valueOf(ReadUtils.readValueFromConsole());
+        int b = Integer.valueOf(ReadUtils.readValueFromConsole());
+
+        int max;
+        int min;
+        if(a < b) {
+            max = b;
+            min = a;
+        } else {
+            max = a;
+            min = b;
+        }
+        int product = 0;
+        for(int i = 0; i < min; i++ ) {
+            product = product + max;
+            System.out.println(product);
+        }
+        System.out.println(a + " * " + b + " = " + product);
+    }
+}
